5e725524a26b26621d87390ab115803a467d3cc7
ryanlukito <ryanlukito13@gmail.com>
2025-06-26 23:31:28 +0800

    add all remaining pages

---
+++ client/src/app/Peta/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Peta = () => {
+  return <div>Peta</div>;
+};
+
+export default Peta;
---
+++ client/src/app/Tim/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Tim = () => {
+  return <div>Tim</div>;
+};
+
+export default Tim;
